7f0c1c006fbe74d4ee8e56cf44676ca379939c30
jmaraya1229 <jessemaraya2013@gmail.com>
2022-04-30 16:41:14 -0700

    edit Container.js

---
+++ src/components/Container.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import Nav from './Nav';
+import Header from './Header';
+import Footer from './Footer';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Projects from './pages/Projects';
+
+export default function Container() {
+    const [currentPage, setCurrentPage] = useState('About');
+
+    const renderPage = () => {
+        if (currentPage === 'About') {
+            return <About />;
+        }
+        if (currentPage === 'Contact') {
+            return <Contact />;
+        }
+        return <Projects />;
+    }
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+            <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+            {renderPage()}
+            <Footer />
+        </div>
+    )
+}

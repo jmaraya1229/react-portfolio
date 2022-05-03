c39ef84b5156876ccedf86989af49063f45f7a01
jmaraya1229 <jessemaraya2013@gmail.com>
2022-05-03 11:31:33 -0700

    update favicon

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,8 +4,9 @@ export default function About() {
     return (
         <div>
             <h1>About Me</h1>
-            <p>
-                Me do code things
+            <img></img>
+            <p>Hello! My name is Jesse Maraya. 
+                I am an aspiring programmer pursuing full stack web development. 
             </p>
         </div>
     );
